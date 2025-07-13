4f9a6185001f809345c4ba6384dfbac1a6f1381c
huy.nguyen3 <mailto:huy.nguyen3@fssc.com.vn>
2025-07-14 00:17:23 +0700

    update

---
+++ src/app/app.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {NzContentComponent, NzFooterComponent, NzHeaderComponent, NzLayoutComponent, NzSiderComponent} from 'ng-zorro-antd/layout';
+import {NzIconDirective} from 'ng-zorro-antd/icon';
+import {NzMenuDirective, NzMenuItemComponent, NzSubMenuComponent} from 'ng-zorro-antd/menu';
+import {NzBreadCrumbComponent, NzBreadCrumbItemComponent} from 'ng-zorro-antd/breadcrumb';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NzLayoutComponent, NzSiderComponent, NzIconDirective, NzMenuItemComponent, NzSubMenuComponent, NzMenuDirective, NzHeaderComponent, NzContentComponent, NzBreadCrumbComponent, NzBreadCrumbItemComponent, NzFooterComponent, NgOptimizedImage],
+  templateUrl: './app.html',
+  standalone: true,
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('about-me');
+}
